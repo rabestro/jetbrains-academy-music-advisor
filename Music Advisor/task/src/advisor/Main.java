84173e1dc187ece268335f64f0b0a85b06face4b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-27 12:44:08 +0200

    Solved stage 2

--- Music Advisor/task/src/advisor/Main.java
+++ Music Advisor/task/src/advisor/Main.java
@@ -3,35 +3,44 @@ package advisor;
 import java.util.Scanner;
 
 public class Main {
+    static boolean auth = false;
+
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
         application:
         for (; ; ) {
             final var command = scanner.nextLine();
             switch (command) {
+                case "auth":
+                    System.out.println("https://accounts.spotify.com/authorize?"
+                            + "client_id=81bcb1ba1c224b74b5f2bed6bb185cad"
+                            + "&redirect_uri=http://localhost:8080&response_type=code\n" +
+                            "---SUCCESS---");
+                    auth = true;
+                    break;
                 case "new":
-                    System.out.println("---NEW RELEASES---\n" +
+                    print("---NEW RELEASES---\n" +
                             "Mountains [Sia, Diplo, Labrinth]\n" +
                             "Runaway [Lil Peep]\n" +
                             "The Greatest Show [Panic! At The Disco]\n" +
                             "All Out Life [Slipknot]");
                     break;
                 case "featured":
-                    System.out.println("---FEATURED---\n" +
+                    print("---FEATURED---\n" +
                             "Mellow Morning\n" +
                             "Wake Up and Smell the Coffee\n" +
                             "Monday Motivation\n" +
                             "Songs to Sing in the Shower");
                     continue;
                 case "categories":
-                    System.out.println("---CATEGORIES---\n" +
+                    print("---CATEGORIES---\n" +
                             "Top Lists\n" +
                             "Pop\n" +
                             "Mood\n" +
                             "Latin");
                     continue;
                 case "playlists Mood":
-                    System.out.println("---MOOD PLAYLISTS---\n" +
+                    print("---MOOD PLAYLISTS---\n" +
                             "Walk Like A Badass  \n" +
                             "Rage Beats  \n" +
                             "Arab Mood Booster  \n" +
@@ -42,5 +51,9 @@ public class Main {
                     break application;
             }
         }
+    }
+
+    static void print(String message) {
+        System.out.println(auth ? message : "Please, provide access for application.");
     }
 }
