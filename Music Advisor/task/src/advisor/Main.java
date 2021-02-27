81dc687bfac38ef9c14f13c7d0a4d2e16cc12d5c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2021-02-27 09:09:15 +0200

    Initial commit

---
+++ Music Advisor/task/src/advisor/Main.java
@@ -0,0 +1,46 @@
+package advisor;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final var scanner = new Scanner(System.in);
+        application:
+        for (; ; ) {
+            final var command = scanner.nextLine();
+            switch (command) {
+                case "new":
+                    System.out.println("---NEW RELEASES---\n" +
+                            "Mountains [Sia, Diplo, Labrinth]\n" +
+                            "Runaway [Lil Peep]\n" +
+                            "The Greatest Show [Panic! At The Disco]\n" +
+                            "All Out Life [Slipknot]");
+                    break;
+                case "featured":
+                    System.out.println("---FEATURED---\n" +
+                            "Mellow Morning\n" +
+                            "Wake Up and Smell the Coffee\n" +
+                            "Monday Motivation\n" +
+                            "Songs to Sing in the Shower");
+                    continue;
+                case "categories":
+                    System.out.println("---CATEGORIES---\n" +
+                            "Top Lists\n" +
+                            "Pop\n" +
+                            "Mood\n" +
+                            "Latin");
+                    continue;
+                case "playlists Mood":
+                    System.out.println("---MOOD PLAYLISTS---\n" +
+                            "Walk Like A Badass  \n" +
+                            "Rage Beats  \n" +
+                            "Arab Mood Booster  \n" +
+                            "Sunday Stroll");
+                    continue;
+                case "exit":
+                    System.out.println("---GOODBYE!---");
+                    break application;
+            }
+        }
+    }
+}
